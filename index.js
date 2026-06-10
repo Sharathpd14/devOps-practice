01e9682c27e11f3cb34dab2dcac15c3e507065d9
Sharathpd14 <sharathpd14@gmail.com>
2026-06-10 20:08:33 +0530

    Version Update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.get("/",(req,res)=>{
-    return res.json({message : "Server is healthy"})
+    return res.json({message : "Server is healthy, version 3.0"})
 })
 
 app.get("/home", (req,res)=>{
